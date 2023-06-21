45fb1f1b38bd64a6307dace49611272863a50d1b
Tomas Strand <tomas@fik1.net>
2023-06-21 10:17:54 +0300

    Fix install of helper scripts

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -18,10 +18,12 @@ $(eval $(kernel-module))
 
 # Maybe not the smoothest way to enable loading of our modules
 define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(BR2_ROOTFS_OVERLAY)/bin/scull_load
-	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(BR2_ROOTFS_OVERLAY)/bin/scull_unload
+	mkdir -p $(BR2_ROOTFS_OVERLAY)/bin
+	$(INSTALL) -m 0755 $(LDD_SRCDIR)/scull/scull_load $(BR2_ROOTFS_OVERLAY)/bin/scull_load
+	$(INSTALL) -m 0755 $(LDD_SRCDIR)/scull/scull_unload $(BR2_ROOTFS_OVERLAY)/bin/scull_unload
 	
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(BR2_ROOTFS_OVERLAY)/bin/module_load
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(BR2_ROOTFS_OVERLAY)/bin/module_unload
+	$(INSTALL) -m 0755 $(LDD_SRCDIR)/misc-modules/module_load $(BR2_ROOTFS_OVERLAY)/bin/module_load
+	$(INSTALL) -m 0755 $(LDD_SRCDIR)/misc-modules/module_unload $(BR2_ROOTFS_OVERLAY)/bin/module_unload
 endef
+
 $(eval $(generic-package))
